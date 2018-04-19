9ed65bc80fef5aef35fe98444ec9d646fbd34caa
Tata Coa <35470096+tatacoa@users.noreply.github.com>
2018-04-19 15:03:55 +0200

    Update Reg_Exe1.R

--- Reg_Exe1.R
+++ Reg_Exe1.R
@@ -1,6 +1,6 @@
-# Ana Maria Sandoval
-# Regression Exercise 1
-
+# Ana Maria Sandoval Jimenez
+# Exercise 1 Regresion
+# Linear Models
 
 
 # exc 5
@@ -12,4 +12,42 @@ var(speed); var(braking)
 cov(speed,braking)
 plot(speed,braking)
 cor(speed,braking)
-lm(speed ~ braking)
+l <- lm(speed ~ braking)
+l$coefficients
+plot(speed,braking)
+abline(l)
+
+data = read.csv2("/Users/tatacoa/Downloads/Miete2003.csv")
+summary(data)
+
+
+
+# Estimate the model for wohnflaeche and nettomiete
+lm2 <- lm(data$wohnflaeche ~ data$nettomiete)
+# calculate the coefficients
+lm2$coefficients
+# draw scatteplots and regression lines
+plot(data$wohnflaeche ~ data$nettomiete)
+abline(lm2, col= "red")
+# determine R-squared.
+summary(lm2)
+
+
+# exc 6
+# consider nettomiete and wohnlage, vizualice them
+plot(data$wohnlage, data$nettomiete)
+boxplot(nettomiete~wohnlage, data=data)
+boxplot(data$nettomiete ~ data$wohnlage, data=data)
+
+# consider bad.kacheln and geh.kueche
+plot(data$bad.kacheln, data$geh.kueche)
+table(data$bad.kacheln, data$geh.kueche)
+
+
+# find the correlation between two binary variables
+library("psych")
+phi(table(data$bad.kacheln, data$geh.kueche))
+
+# use chi-squared test on binary variabes
+chisq.test(table(data$bad.kacheln, data$geh.kueche))
+
